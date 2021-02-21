01307a5173969701692206efe8f41a0e0f29ea73
esionascimento <esio.392@protonmail.com>
2021-02-21 02:55:17 -0400

    Parte 2 e 3 do exercício de fixação, 4.1 bloco 1

---
+++ Introduction/bloco_4/fixacao/parte2.js
@@ -0,0 +1,12 @@
+const name = 'Esio';
+const birthCity = 'Porto Velho';
+let birthYear = 1998;
+
+console.log(name, birthCity, birthYear);
+
+birthYear = 2020;
+
+console.log(birthYear);
+
+//birtCity = 1111;
+//erro const nao pode ser reatribuida
---
+++ Introduction/bloco_4/fixacao/parte3.js
@@ -0,0 +1,9 @@
+let base = 5;
+let altura = 8;
+let area = base*altura;
+
+console.log(area);
+
+let perimetro = base**2;
+
+console.log(perimetro);
