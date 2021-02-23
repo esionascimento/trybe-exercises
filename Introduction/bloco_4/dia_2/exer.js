774b1412e09618db2631c717520e3a90da15c16c
esionascimento <esio.392@protonmail.com>
2021-02-22 21:33:41 -0400

    2. soma do array

--- Introduction/bloco_4/dia_2/exer.js
+++ Introduction/bloco_4/dia_2/exer.js
@@ -1,8 +1,11 @@
 const { Collection } = require("mongoose");
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let a;
+let a, soma=0;
 
 for (a=0; a<numbers.length; a++){
     console.log(numbers[a]);
+    soma+=numbers[a];
 }
+
+console.log(soma);
