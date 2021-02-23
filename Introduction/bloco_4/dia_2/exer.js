555ac44967d9cac6da2de18a49fd06732150f2da
esionascimento <esio.392@protonmail.com>
2021-02-22 22:04:39 -0400

    6. qtd numeros impares no array

--- Introduction/bloco_4/dia_2/exer.js
+++ Introduction/bloco_4/dia_2/exer.js
@@ -1,9 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let a, soma=0, maior=-9999;
+let a, soma=0, resto, contImpar=0, maior=-9999;
 
 for (a=0; a<numbers.length; a++){
     console.log(numbers[a]);
     soma+=numbers[a];
+    resto=numbers[a]%2;
+    if(resto!=0){
+        contImpar++;
+    }
     if(numbers[a]>maior){
         maior=numbers[a];
     }
@@ -17,3 +21,4 @@ if(soma>20){
     console.log('4. Valor menor ou igual a 20');
 }
 console.log(`5. Maior: ${maior}`);
+console.log(`6. ${contImpar}`);
