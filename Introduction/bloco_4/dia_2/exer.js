979f18e2de627c52850d956997e10b1ffc73f7e4
esionascimento <esio.392@protonmail.com>
2021-02-22 21:59:08 -0400

    4. valor final maior ou menor que 20

--- Introduction/bloco_4/dia_2/exer.js
+++ Introduction/bloco_4/dia_2/exer.js
@@ -1,5 +1,3 @@
-const { Collection } = require("mongoose");
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let a, soma=0;
 
@@ -7,6 +5,11 @@ for (a=0; a<numbers.length; a++){
     console.log(numbers[a]);
     soma+=numbers[a];
 }
-
-console.log(soma);
-console.log(soma/numbers.length);
+console.log(`----------`);
+console.log(`2. ${soma}`);
+console.log(`3. ${soma/numbers.length}`);
+if(soma>20){
+    console.log('4. Valor maior que 20');
+}else{
+    console.log('4. Valor menor ou igual a 20');
+}
