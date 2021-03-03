9669228f1141403df52ab30595f400a760d8010c
esionascimento <esio.392@protonmail.com>
2021-03-03 17:07:54 -0300

    parte 1 - getElementById exercises 3

--- Introduction/bloco_5/conteudos/parte-2-seletores-de-elementos/script.js
+++ Introduction/bloco_5/conteudos/parte-2-seletores-de-elementos/script.js
@@ -1,2 +1,3 @@
-document.getElementById("page-title").innerText="Xablau";
+document.getElementById("page-title").innerHTML="Xablau";
 document.getElementById("second-paragraph").innerText="1 xablau 2 xablau 3 xablau";
+document.getElementById("subtitle").innerHTML="subs";
