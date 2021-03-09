26d620e49ab8e68d5328707b932e59b24ed54cfe
esionascimento <esio.392@protonmail.com>
2021-03-09 01:59:32 -0300

    concluido, modificacao linha 54

--- Introduction/bloco_5/dia_4/script.js
+++ Introduction/bloco_5/dia_4/script.js
@@ -8,6 +8,8 @@ function addInput(nTag, nClass, nPlace) {
 }
 addInput('input', 'inputCorTela', 'cor fundo tela');
 addInput('input', 'inputCorP', 'cor paragrafo');
+addInput('input', 'inputEspacamento', 'Espacamento');
+addInput('input', 'inputTipoFonte', 'Tipo da fonte');
 //----------------------------------------------------------
 let getCorFundoTela = document.querySelector('.inputCorTela');
 getCorFundoTela.addEventListener('change', alterarBackgroundBody);
@@ -49,17 +51,35 @@ function confirmaAlteracao(e) {
 var option = select.options[select.selectedIndex];
 
 console.log(option.value); // pt */
-
+let alteraPara = document.querySelectorAll('.para');
+let alterarP = document.getElementById('paragrafo');
+let alterarP2 = document.getElementById('paragrafo2');
 
 function update() {
-  let alterarP = document.getElementById('paragrafo');
-  let alterarP2 = document.getElementById('paragrafo2');
   var select = document.getElementById('language');
   var option = select.options[select.selectedIndex];
-  
-
   alterarP.style.fontSize = option.value;
   alterarP2.style.fontSize = option.value;
-
 }
 update();
+function updateEspacamento() {
+  var select = document.getElementById('espacamento');
+  var option = select.options[select.selectedIndex];
+  alterarP.style.letterSpacing = option.value;
+  alterarP2.style.letterSpacing = option.value;
+}
+updateEspacamento();
+
+let getEspacamento = document.querySelector('.inputEspacamento');
+let getTipoFonte = document.querySelector('.inputTipoFonte');
+getTipoFonte.addEventListener('change', alteraTipoFonte);
+getEspacamento.addEventListener('change', alteraEspacamento);
+
+function alteraTipoFonte() {
+  alterarP.style.fontFamily = getTipoFonte.value;
+  alterarP2.style.fontFamily = getTipoFonte.value;
+}
+
+function alteraEspacamento() {
+
+}
