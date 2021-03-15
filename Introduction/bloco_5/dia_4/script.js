5419def3cbf76d5870a6162cec6a5707390255a2
esionascimento <esio.392@protonmail.com>
2021-03-15 18:40:53 -0300

    dia_4, tentativa de salvamento diferente

--- Introduction/bloco_5/dia_4/script.js
+++ Introduction/bloco_5/dia_4/script.js
@@ -1,62 +1,61 @@
+/* if (localStorage.tamFonte) {
+  let recuperarDados = function() {
+  let recuperaFont = localStorage.getItem('tamFont')
+  document.getElementById('tamFont').value = localStorage.tamFont;
+} */
+let corFundoTela;
+let CorTxt;
+let tamFonte;
+let espacamento;
+let tipoFonte;
+let aux;
 let leBody = document.getElementById('body');
-
-function addInput(nTag, nClass, nPlace) {
-  let addInputTela = document.createElement(nTag);
-  addInputTela.className = nClass;
-  addInputTela.placeholder = nPlace;
-  leBody.appendChild(addInputTela);
-}
-addInput('input', 'inputCorTela', 'cor fundo tela');
-addInput('input', 'inputCorP', 'cor paragrafo');
-addInput('input', 'inputEspacamento', 'Espacamento');
-addInput('input', 'inputTipoFonte', 'Tipo da fonte');
-//----------------------------------------------------------
-let getCorFundoTela = document.querySelector('.inputCorTela');
-getCorFundoTela.addEventListener('change', alterarBackgroundBody);
-let getCorParagrafo = document.querySelector('.inputCorP');
-getCorParagrafo.addEventListener('change', alterarCorP);
-//----------------------------------------------------------
-function alterarBackgroundBody() {
-  leBody.style.backgroundColor = getCorFundoTela.value;
-}
-function alterarCorP() {
-  leBody.style.color = getCorParagrafo.value;
-}
-/* function getValor(confirmado) {
-  let getCor = confirmado.value;
-  alterarBackgroundBody(getCor, confirmado)
-}
-function confirmaAlteracao(e) {
-  let str = document.querySelectorAll('.input');
-  for(let i=0; i<3; i++){
-    let aux = [];
-    aux = str[i];
-    let words = aux.className.split(' ');
-
-    if(words[1] === 'inputCorP'){
-      getValor(words[1])  
-      console.log(words[1]);
-    }else{
-      getValor()  
-    }
-  }
-  
-  /* if(e.which == 13){
-     funCompromisso();
-  } */
-
-//2
-
-/* var select = document.getElementById('language');
-var option = select.options[select.selectedIndex];
-
-console.log(option.value); // pt */
-let alteraPara = document.querySelectorAll('.para');
 let alterarP = document.getElementById('paragrafo');
 let alterarP2 = document.getElementById('paragrafo2');
+let buttonTela = document.querySelector('#btn-corTela');
+let buttonTexto = document.querySelector('#btn-corTexto');
+let inputTela = document.querySelector('#corFundo');
+let inputTxt = document.querySelector('#corTexto');
+if(localStorage.corTela){
+  aux = localStorage.getItem('corTela');
+  leBody.style.backgroundColor = aux;
+}
 
-function update() {
-  var select = document.getElementById('language');
+/* let exem2 = localStorage.getItem('tamFonte');
+console.log(exem2); */
+/*   
+alterarP.style.fontSize = exem2;
+alterarP2.style.fontSize = exem2; */
+
+buttonTela.addEventListener('click', alteraTela);
+buttonTexto.addEventListener('click', alteraTexto);
+
+function alteraTela() {
+  let tela = inputTela.value;
+  if(tela !== 'undefined'){
+    console.log(tela);
+    aux = tela;
+  }else{
+    aux = corFundoTela;
+  }
+  leBody.style.backgroundColor = aux;
+}
+function alteraTexto() {
+  let texto = inputTxt.value;
+  if(texto !== 'undefined'){
+    aux = texto;
+  }else{
+    aux=CorTxt;
+  }
+  leBody.style.color = aux;
+}
+let corTela = localStorage.setItem("corTela", 'red');
+let CorTxt = localStorage.setItem("corTxt", 'green');
+
+//let alteraPara = document.querySelectorAll('.para');
+
+/* function update() {
+  var select = document.getElementById('tamFont');
   var option = select.options[select.selectedIndex];
   alterarP.style.fontSize = option.value;
   alterarP2.style.fontSize = option.value;
@@ -70,16 +69,21 @@ function updateEspacamento() {
 }
 updateEspacamento();
 
-let getEspacamento = document.querySelector('.inputEspacamento');
-let getTipoFonte = document.querySelector('.inputTipoFonte');
-getTipoFonte.addEventListener('change', alteraTipoFonte);
-getEspacamento.addEventListener('change', alteraEspacamento);
 
-function alteraTipoFonte() {
+function alteraTipoFonte(e) {
   alterarP.style.fontFamily = getTipoFonte.value;
   alterarP2.style.fontFamily = getTipoFonte.value;
 }
 
-function alteraEspacamento() {
-
+let salvarDados = function() {
+  leBody.style.backgroundColor = getCorFundoTela.value;
+  let salvarBody = document.querySelector('body').value;
+  let tamFont = document.getElementById('tamFont').value;
+  let tipoFont = document.getElementById('espacamento').value;
+  localStorage.setItem('body', salvarBody);
+  localStorage.setItem('tamFonte', tamFont);
+  localStorage.setItem('tipoFont', tipoFont);
 }
+document.onchange = salvarDados;
+
+ */
