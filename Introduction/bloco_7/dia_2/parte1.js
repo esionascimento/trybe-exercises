f971cb47a703886c5bc4b73de2626c488a0e8b89
esionascimento <esio.392@protonmail.com>
2021-03-24 21:59:55 -0300

    7.2 concluido

--- Introduction/bloco_7/dia_2/parte1.js
+++ Introduction/bloco_7/dia_2/parte1.js
@@ -33,25 +33,24 @@ const order = {
     total: 60,
   },
 };
-
-/* const customerInfo = (order) => {
-  // Adicione abaixo as informações necessárias.
+/* 1-Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701". */
+const customerInfo = (order) => {
   const aux = `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`;
-  console.log(aux);
+  return(aux);
 }
+console.log(`${customerInfo(order)}
+`);
 
-customerInfo(order); */
-
+/* 2-Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00." */
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
   const clone = Object.assign({}, order);
   clone.name = 'Luiz Silva';
   clone.payment.total = 50;
   clone.order.pizza['muzzarella'] = 'muzzarella';
   clone.order.pizza['calabresa'] = 'calabresa';
-
   const aux = `Olá ${clone.name}, o total do seu pedido de ${clone.order.pizza.muzzarella}, ${clone.order.pizza.calabresa} e ${clone.order.drinks.coke.type} é R$ ${clone.payment.total},00.`;
-  console.log(aux);
+  return (aux);
 }
 
-orderModifier(order);
+console.log(`${orderModifier(order)}
+`);
