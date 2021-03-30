5786e0668493bd83313f920804b94c47e47a0f38
esionascimento <esio.392@protonmail.com>
2021-03-30 08:35:48 -0300

    concluido exercise 4

--- Introduction/bloco_8/dia_1/exercise4.js
+++ Introduction/bloco_8/dia_1/exercise4.js
@@ -1,2 +1,20 @@
-const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const checkGabarito = (certas, aluno) => {
+  let pontos = 0;
+  for (let index = 0; index < certas.length; index += 1) {
+    if (certas[index] === aluno[index]) {
+      pontos += 1;
+    } else if (certas[index] !== aluno[index]) {
+      pontos -= 0.5;
+    }
+  }
+  return pontos;
+}
+
+const resultadoFinal = (checkCertas, checkAluno, verifica) => {
+  return verifica(checkCertas, checkAluno);
+}
+
+const rightAnswers = [  'A',  'C',  'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+console.log(resultadoFinal(rightAnswers, studentAnswers, checkGabarito));
